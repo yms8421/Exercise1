a8af8c1cf6790175c8fdcf1bad9e8f4b87a964e7
yms8421 <46646219+yms8421@users.noreply.github.com>
2019-01-27 12:04:08 +0300

    Add files via upload

---
+++ BilgeAdam.Exercises1/BilgeAdam.LeapYear/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BilgeAdam.LeapYear
+{
+    class Program
+    {
+        //CQRS: Command Query Responsibility Segregation
+        static void Main(string[] args)
+        {
+            var currentYear = DateTime.Now.Year;
+            var year = currentYear - 200;
+            for (; year < currentYear; year++)
+            {
+                if (year % 100 == 0 && year % 400 != 0)
+                {
+                    continue;
+                }
+                if (year % 4 == 0)
+                {
+                    Console.WriteLine(year);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BilgeAdam.Exercises1/BilgeAdam.PrimeNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BilgeAdam.PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2, 3, 5, 7, 11, 13, 17, 19 ....
+            var exception = 2;
+            Console.Write(exception);
+            for (var number = 3; number < 100; number++)
+            {
+                var half = number / 2;
+                var isDivided = false;
+                for (var d = 2; d < half; d++)
+                {
+                    if (number % d == 0)
+                    {
+                        isDivided = true;
+                        break;
+                    }
+                }
+                if (!isDivided)
+                {
+                    Console.Write(" - {0}", number);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
